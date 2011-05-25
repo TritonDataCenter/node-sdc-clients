792cf36ee2eebed04d98387b821edc7c5bcf464c
Mark Cavage <mark.cavage@joyent.com>
2011-05-25 09:13:55 -0700

    Initial creation

---
+++ tst/empty.test.js
@@ -0,0 +1,17 @@
+// Copyright 2011 Joyent, Inc.  All rights reserved.
+
+
+
+exports.setUp = function(test, assert) {
+  test.finish();
+};
+
+
+exports.test_empty = function(test, assert) {
+  test.finish();
+};
+
+
+exports.tearDown = function(test, assert) {
+  test.finish();
+};
