ad4a41eb9b1480edd39a2a63a4ed64082105d9fa
Pedro P. Candel <pedro@joyent.com>
2012-11-08 17:58:07 +0100

    PUBAPI-468: ufds.listVms/ufds.user.listVms method added.

--- test/ufds.test.js
+++ test/ufds.test.js
@@ -217,6 +217,55 @@ exports.testCrudLimit = function (test) {
 };
 
 
+exports.testsListVms = function (test) {
+    var VM_ONE = {
+        objectclass: 'vmusage',
+        ram: 1024,
+        quota: 10240,
+        uuid: uuid(),
+        image_uuid: uuid(),
+        image_os: 'smartos',
+        image_name: 'percona',
+        billing_id: uuid()
+    };
+
+    var VM_TWO = {
+        objectclass: 'vmusage',
+        ram: 2048,
+        quota: 10240,
+        uuid: uuid(),
+        image_uuid: uuid(),
+        image_os: 'smartos',
+        image_name: 'smartos',
+        billing_id: uuid()
+    };
+
+
+    var VM_FMT = 'vm=%s, uuid=%s, ou=users, o=smartdc';
+    ufds.add(util.format(VM_FMT, VM_ONE.uuid, ADMIN_UUID), VM_ONE,
+            function (err) {
+        test.ifError(err, 'Add VM_ONE error');
+        ufds.add(util.format(VM_FMT, VM_TWO.uuid, ADMIN_UUID), VM_TWO,
+            function (err2) {
+            test.ifError(err2, 'Add VM_TWO error');
+            ufds.listVms(ADMIN_UUID, function (err3, vms) {
+                test.ifError(err3, 'Error listing Vms');
+                test.ok(Array.isArray(vms));
+                ufds.getUser('admin', function (err4, user) {
+                    test.ifError(err4, 'listVms getUser error');
+                    test.ok(user);
+                    user.listVms(function (err5, vms2) {
+                        test.ifError(err5, 'list user vms error');
+                        test.ok(Array.isArray(vms2));
+                        test.ok(vms2.length >= 2);
+                        test.done();
+                    });
+                });
+            });
+        });
+    });
+};
+
 exports.tearDown = function (callback) {
     ufds.close(function () {
         callback();
