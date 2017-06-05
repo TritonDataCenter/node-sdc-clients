78624de976118140649d59a06471dacd289a7ac3
Josh Wilsdon <jwilsdon@joyent.com>
2017-06-05 13:45:02 -0700

    VOLAPI-50 Add ListVolumeSizes endpoint Reviewed by: Julien Gilli <julien.gilli@joyent.com> Approved by: Julien Gilli <julien.gilli@joyent.com>
    merges PR https://github.com/joyent/node-sdc-clients/pull/11

--- test/volapi.test.js
+++ test/volapi.test.js
@@ -5,11 +5,12 @@
  */
 
 /*
- * Copyright (c) 2016, Joyent, Inc.
+ * Copyright (c) 2017, Joyent, Inc.
  */
 
 var bunyan = require('bunyan');
-var restify = require('restify');
+var restify = require('restify-clients');
+var test = require('tape');
 
 var VOLAPI = require('../lib/index').VOLAPI;
 
@@ -18,81 +19,104 @@ var VOLAPI_URL = 'http://' + VOLAPI_IP;
 
 var volApiClient;
 
-exports.setUp = function (callback) {
-    var log = new bunyan.createLogger({
-        name: 'volapi_unit_test',
-        stream: process.stderr,
-        level: (process.env.LOG_LEVEL || 'info'),
-        serializers: restify.bunyan.serializers
-    });
+test('volapi', function (tt) {
+    tt.test(' setup', function (t) {
+        var log = new bunyan.createLogger({
+            name: 'volapi_unit_test',
+            stream: process.stderr,
+            level: (process.env.LOG_LEVEL || 'info'),
+            serializers: restify.bunyan.serializers
+        });
 
-    volApiClient = new VOLAPI({
-        url: VOLAPI_URL,
-        version: '^1',
-        userAgent: 'node-sdc-clients-volapi-tests',
-        retry: {
-            retries: 1,
-            minTimeout: 1000
-        },
-        log: log
-    });
-
-    callback();
-};
-
-exports.test_ping = function (t) {
-    volApiClient.ping(function (err) {
-        t.ifError(err);
-        t.done();
-    });
-};
-
-exports.test_list_volumes = function (t) {
-    volApiClient.listVolumes(function (err, objs) {
-        t.ifError(err);
-        t.ok(objs);
-        t.ok(Array.isArray(objs));
-        t.ok(objs.length > 0);
-        t.ok(objs[0].name);
-        t.ok(objs[0].type);
-        t.done();
-    });
-};
-
-exports.test_api_version_needs_to_be_specified = function (t) {
-    var apiClient;
-    t.throws(function badClient() {
-        apiClient = new VOLAPI({
+        volApiClient = new VOLAPI({
             url: VOLAPI_URL,
-            userAgent: 'node-sdc-clients-volapi-tests'
+            version: '^1',
+            userAgent: 'node-sdc-clients-volapi-tests',
+            retry: {
+                retries: 1,
+                minTimeout: 1000
+            },
+            log: log
+        });
+
+        t.ok(volApiClient, 'created volapi client');
+
+        t.end();
+    });
+
+    tt.test(' ping', function (t) {
+        volApiClient.ping(function (err) {
+            t.ifErr(err, 'ping should succeed');
+            t.end();
         });
     });
 
-    if (apiClient) {
-        apiClient.close();
-    }
-
-    t.done();
-};
-
-exports.test_api_version_star_not_allowed = function (t) {
-    var apiClient;
-    t.throws(function badClient() {
-         apiClient = new VOLAPI({
-            url: VOLAPI_URL,
-            version: '*',
-            userAgent: 'node-sdc-clients-volapi-tests'
+    tt.test(' list volumes', function (t) {
+        volApiClient.listVolumes(function (err, objs) {
+            t.ifErr(err, 'listVolumes should succeed');
+            t.ok(objs, 'listVolumes should return object');
+            t.ok(Array.isArray(objs), 'listVolumes object should be an array');
+            if (objs.length > 0) {
+                // We can only check the volumes if some have been created and
+                // this test does not create any so we cannot (yet) rely on
+                // there being any.
+                t.ok(objs[0].name, 'volume[0] should have a .name');
+                t.equal(objs[0].type, 'tritonnfs',
+                    'volume[0] should have type tritonnfs');
+            }
+            t.end();
         });
     });
 
-    if (apiClient) {
-        apiClient.close();
-    }
+    tt.test(' list volume sizes', function (t) {
+        volApiClient.listVolumeSizes(function (err, objs) {
+            t.ifError(err, 'ListVolumeSizes should succeed');
+            t.ok(objs, 'should have received object from ListVolumeSizes');
+            t.ok(Array.isArray(objs), 'volume sizes object should be array');
+            t.ok(objs.length > 0,
+                'volume sizes array should have at least 1 size object');
+            t.ok(objs[0].size, 'expected value for size, got: ' + objs[0].size);
+            t.ok(objs[0].description, 'expected value for description, got: ' +
+                objs[0].description);
+            t.end();
+        });
+    });
 
-    t.done();
-};
+    tt.test(' api version needs to be specified', function (t) {
+        var apiClient;
+        t.throws(function badClient() {
+            apiClient = new VOLAPI({
+                url: VOLAPI_URL,
+                userAgent: 'node-sdc-clients-volapi-tests'
+            });
+        });
 
-exports.tearDown = function (callback) {
-    volApiClient.close();
-    callback();
-};
+        if (apiClient) {
+            apiClient.close();
+        }
+
+        t.end();
+    });
+
+    tt.test(' api version star not allowed', function (t) {
+        var apiClient;
+        t.throws(function badClient() {
+             apiClient = new VOLAPI({
+                url: VOLAPI_URL,
+                version: '*',
+                userAgent: 'node-sdc-clients-volapi-tests'
+            });
+        });
+
+        if (apiClient) {
+            apiClient.close();
+        }
+
+        t.end();
+    });
+
+    tt.test(' teardown', function (t) {
+        volApiClient.close();
+        t.end();
+    });
+});
